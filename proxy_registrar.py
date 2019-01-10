09fae3b332b33664def0dd65dc2f381a472250b3
Claudia Alvarez Bravo <claualbr@alpha12.aulas.gsyc.urjc.es>
2019-01-10 13:30:07 +0100

    introduce cabecera de que ha pasado por el proxy

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -76,31 +76,51 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         for user in user_del:
             del self.dicc_reg[user]
 
+    def envio_client(self, ip, port, linea):
+        """Envio mensajes al uaclient."""
+        self.wfile.write(bytes(linea, 'utf-8'))
+        print('mandamos al cliente: ', linea)
+        log_send = linea.replace("\r\n", " ")
+        log('Sent to ' + ip + ':' + str(port) + ': ' + log_send, LOG_PATH)
+
     def envio_destino(self, ip, port, mensaje):
         """Envio los mensajes al uaserver."""
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
             my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             my_socket.connect((ip, port))
 
-            print('mandamos al servidor: ', mensaje)
-            my_socket.send(bytes(mensaje, 'utf-8'))
-            mensaje = mensaje.replace("\r\n", " ")
-            log('Sent to ' + ip + ':' + str(port) + ': ' + mensaje, LOG_PATH)
+            mens = mensaje.split('\r\n\r\n')
+            mens_proxy = (mens[0] + '\r\nVia: SIP/2.0/UDP ' + IP + ':' +
+                         str(PORT_SERVER) + '\r\n\r\n' + mens[1])
+            print('mandamos al servidor: ', mens_proxy)
+            my_socket.send(bytes(mens_proxy, 'utf-8'))
+            mens_proxy = mens_proxy.replace("\r\n", " ")
+            log('Sent to ' + ip + ':' + str(port) + ': ' +
+                mens_proxy, LOG_PATH)
 
             try:
                 data = my_socket.recv(1024).decode('utf-8')
+                print('recibimos del servidor: ', data)
             except ConnectionRefusedError:
                 log("Error: No server listening at " + ip +
                     " port " + str(port), LOG_PATH)
-            print('recibimos del servidor: ', data)
-            return data
-
-    def envio_client(self, ip, port, linea):
-        """Envio mensajes al uaclient."""
-        self.wfile.write(bytes(linea, 'utf-8'))
-        print('mandamos al cliente: ', linea)
-        log_send = linea.replace("\r\n", " ")
-        log('Sent to ' + ip + ':' + port + ': ' + log_send, LOG_PATH)
+            recb = data.split()
+            env_proxy = ''
+            try:
+                if recb[7] == '200':
+                    recb_proxy = data.split('\r\n\r\n')
+                    env_proxy = (recb_proxy[0] + '\r\n\r\n' +recb_proxy[1] +
+                                 '\r\n\r\n' + recb_proxy[2] +
+                                 '\r\nVia: SIP/2.0/UDP ' + IP + ':' +
+                                 str(PORT_SERVER) + '\r\n\r\n' + recb_proxy[3])
+            except IndexError:
+                pass
+            if env_proxy != '':
+                print('hola')
+                log_send = env_proxy.replace("\r\n", " ")
+                log('Received from ' + ip + ':' +
+                    str(port) + ': ' + log_send, LOG_PATH)
+                self.envio_client(ip, port, env_proxy)
 
     def user_not_found(self):
         """Mensaje de usuario no encontrado."""
@@ -163,7 +183,11 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                                            'puerto': port, 'registro': now}
                     linea_send = "SIP/2.0 200 OK\r\n\r\n"
                 else:
-                    linea_send = "Error: contraseña incorrecta\r\n\r\n"
+                    self.nonce[user] = str(random.randint(0, 100000000))
+                    linea_send = ('SIP/2.0 401 Unauthorized\r\n' +
+                                  'WWW Authenticate: Digest ' +
+                                  'nonce="' + self.nonce[user] +
+                                  '"\r\n\r\n')
                 self.envio_client(ip_client, port_client, linea_send)
             elif line[0] == 'INVITE':
                 linea_recb = linea.replace("\r\n", " ")
@@ -175,12 +199,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                     if server in self.dicc_reg.keys():
                         ip_destino = self.dicc_reg[server]['ip']
                         port_destino = int(self.dicc_reg[server]['puerto'])
-                        linea_send = self.envio_destino(ip_destino,
-                                                        port_destino, linea)
-                        log_send = linea_send.replace("\r\n", " ")
-                        log('Received from ' + ip_client + ':' +
-                            str(port_destino) + ': ' + log_send, LOG_PATH)
-                        self.envio_client(ip_client, port_client, linea_send)
+                        self.envio_destino(ip_destino, port_destino, linea)
                     else:
                         self.user_not_found()
                 else:
@@ -204,12 +223,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 if server in self.dicc_reg.keys():
                     ip_destino = self.dicc_reg[server]['ip']
                     port_destino = int(self.dicc_reg[server]['puerto'])
-                    linea_send = self.envio_destino(ip_destino,
-                                                    port_destino, linea)
-                    log_send = linea_send.replace("\r\n", " ")
-                    log('Received from ' + ip_client + ':' +
-                        str(port_destino) + ': ' + log_send, LOG_PATH)
-                    self.envio_client(ip_client, port_client, linea_send)
+                    self.envio_destino(ip_destino, port_destino, linea)
                 else:
                     self.user_not_found()
             elif line[0] != ('REGISTER', 'INVITE', 'ACK', 'BYE'):
